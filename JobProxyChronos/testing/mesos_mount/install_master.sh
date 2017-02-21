b9355b4545c5963239d66410e89406a67505fd92
Maximilian Wiens <mwiens@cebitec.uni-bielefeld.de>
2017-02-21 16:36:57 +0100

    feat(vagrant): Added automatic test run script, fixed irregular bug while booting VM

--- JobProxyChronos/testing/mesos_mount/install_master.sh
+++ JobProxyChronos/testing/mesos_mount/install_master.sh
@@ -7,6 +7,7 @@ CODENAME=$(lsb_release -cs)
 echo "deb http://repos.mesosphere.io/${DISTRO} ${CODENAME} main" | sudo tee /etc/apt/sources.list.d/mesosphere.list
 
 sudo apt-get -y update
+sudo apt-get -y upgrade
 sudo apt-get install -y python2.7
 sudo apt-get install -y docker.io
 sudo apt-get install -y mesos zookeeperd zookeeper zookeeper-bin
@@ -34,6 +35,8 @@ echo manual | sudo tee /etc/init/mesos-slave.override
 sudo /usr/share/zookeeper/bin/zkServer.sh restart
 sleep 10
 sudo service mesos-master start
+sleep 10
+sudo service docker restart
 sleep 10
 sudo docker run -d --net=host -e PORT0=8081 -e PORT1=8082 mesosphere/chronos:v3.0.0 --zk_hosts 10.0.0.2:2181 --master zk://10.0.0.2:2181/mesos
 
